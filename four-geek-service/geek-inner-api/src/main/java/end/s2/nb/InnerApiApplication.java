2691c90b5fb18dd1e2d4e841bbf943a86d971999
fcx <97356403@qq.com>
2020-03-15 22:16:25 +0800

    测试
    测试

--- four-geek-service/geek-inner-api/src/main/java/end/s2/nb/InnerApiApplication.java
+++ four-geek-service/geek-inner-api/src/main/java/end/s2/nb/InnerApiApplication.java
@@ -6,7 +6,6 @@ import com.sun.xml.internal.ws.api.model.wsdl.WSDLOutput;
 public class InnerApiApplication {
     public static void main(String[] args){
         System.out.println();
-        System.out.println("123");
     }
 
 }
