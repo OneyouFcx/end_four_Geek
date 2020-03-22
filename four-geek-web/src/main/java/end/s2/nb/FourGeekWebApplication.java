ac17eaab734ba0a6533366fb1b3825694e92d413
Yyyjs <17295957@qq.com>
2020-03-22 21:21:21 +0800

    yyy测试1

--- four-geek-web/src/main/java/end/s2/nb/FourGeekWebApplication.java
+++ four-geek-web/src/main/java/end/s2/nb/FourGeekWebApplication.java
@@ -12,6 +12,7 @@ import org.springframework.context.annotation.ComponentScans;
 public class FourGeekWebApplication {
     // user:chh
     // user:yhh
+    // 测试
     // user:fcx
     public static void main(String[] args) {
         SpringApplication.run(FourGeekWebApplication.class, args);
