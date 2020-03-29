857bc1c8be7d0d0cecd414dbd86fcaf2573a023d
Yyyjs <17295957@qq.com>
2020-03-29 20:38:16 +0800

    。。。

--- four-geek-service/geek-service/page-service/src/main/java/end/s2/nb/PageServiceApplication.java
+++ four-geek-service/geek-service/page-service/src/main/java/end/s2/nb/PageServiceApplication.java
@@ -4,7 +4,7 @@ package end.s2.nb;
 public class PageServiceApplication {
     //杨园不聪明
     //- -
-    //**
+    //。。。
     public static void main(String[] args) {
         System.out.println();
     }
